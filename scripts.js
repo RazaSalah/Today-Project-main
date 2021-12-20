8f00bd8ad488703d44f736f19b70349c225fca51
RazaSalah <razansalah41@gmail.com>
2021-12-20 22:58:45 +0300

    add

--- scripts.js
+++ scripts.js
@@ -50,4 +50,61 @@ document.querySelector(".search-bar").addEventListener("keyup" , function(event)
 weather.fetchWeather("Dammam")
 
 
+const APINews ="5a81862cb1fd44e9abafb1890b7e2ce9"
+    fetch( "https://newsapi.org/v2/everything?q=all&from=2021-12-20&sortBy=popularity&apiKey=5a81862cb1fd44e9abafb1890b7e2ce9")
+
+      .then((response) => response.json())
+      .then((data) => this.displayNews(data.articles));
+  
+  function displayNews(data) {
+    data.map((productsValue , i) =>{
+    const productBox = document.querySelector("#productCard")
+    const card = document.createElement("div"); 
+  
+    const productCard = `
+    <div class="d-flex justify-content-center row mb-5">
+    <div class="col-md-10">
+        <div class="row p-2 bg-white border rounded"> 
+            <div class="col-md-3 mt-1">
+               <img id="news-img"class="img-fluid img-responsive rounded product-image" src="${data[i].urlToImage}"> 
+            </div>
+            <div class="col-md-7 mt-1">
+                 <h5 id="news-title">${data[i].title}</h5>
+                <p class="text-justify para mb-0" id="news-desc">${data[i].description}<br><br></p>
+                <a class="text-justify "href="${data[i].url}">More Details</a>
+            </div>
+                  
+            </div>
+        </div>
+        </div>
+        </div>`;
+         card.innerHTML+= productCard;
+         productBox.appendChild(card)
+  })
+}
+  
+function retrieve(){
+  const searchForm = document.querySelector("#news-search-bar").value;
+  
+  productCard.innerHTML = ``  
+ let url = `https://newsapi.org/v2/everything?q=${searchForm}&from=2021-12-20&sortBy=popularity&apiKey=${APINews}`
+   fetch(url).then((response) => response.json())
+   .then((data) => displayNews(data.articles));
+}
+    
+
+document.querySelector("#news-submit").addEventListener("click" , function(){
+  retrieve();
+
+})
+
+document.querySelector("#news-search-bar").addEventListener("keyup" , function(event){
+    
+    if(event.key =="Enter"){
+        retrieve();
+        }
+
+});
+
+
 
